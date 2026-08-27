275932793f6a11dc627636fd1868a7f4bf345a8a
Fahmida Afroz Moni <fahmidaafrozmoni2928@gmail.com>
2026-08-27 16:10:32 +0600

    change fatching

--- src/app/all-animals/page.jsx
+++ src/app/all-animals/page.jsx
@@ -1,14 +1,23 @@
 import AnimalCard from "@/Components/AnimalCard";
 
+import fs from "fs";
+import path from "path";
 
 const AllAnimalsPage = async () => {
-   const res = await fetch("/data/products.json");
+  // const res = await fetch("http://localhost:3000/data/products.json");
 
-   const animals = await res.json();
-  console.log(animals);
+  // const animals = await res.json();
+ // console.log(animals);
+
+  const filePath = path.join(
+    process.cwd(),
+    "public",
+    "data",
+    "products.json");
 
   
-
+ const fileData = fs.readFileSync(filePath, "utf-8");
+  const animals = JSON.parse(fileData);
   return (
     <div className="bg-green-100 w-full mt-5 py-8">
       <h2 className="text-center font-bold text-4xl">
