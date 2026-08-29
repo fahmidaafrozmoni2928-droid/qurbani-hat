ba2e33777b69dd375bb3f01e61934308a5efab58
Fahmida Afroz Moni <fahmidaafrozmoni2928@gmail.com>
2026-08-29 09:05:50 +0600

    auth.js changed

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -4,7 +4,7 @@ export const authClient = createAuthClient({
     baseURL: process.env.BETTER_AUTH_URL,
 })
 
-export const { signIn, signUp, useSession } = createAuthClient()
+export const { signIn, signUp, useSession } = authClient;
 
 
 
